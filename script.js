2791fdbfde0dcb14e6b4ef27147324af68cfb4ee
DaniloII09 <00377223@uca.edu.sv>
2025-02-27 12:58:00 -0600

    :sparkles: :lipstick: add: search filter and background redesign

--- script.js
+++ script.js
@@ -1,27 +1,94 @@
 const charactersContainer = document.getElementById("characters-container");
+const characterSearch = document.getElementById("character-search");
+
+let characters = [];
+
+let isEditing = false;
+let currentEditing = null;
+
+const createCharacter = (
+  characterImage,
+  characterName,
+  characterStatus,
+  characterSpecies
+) => {
+  const card = document.createElement("div");
+  card.className = "card";
+
+  const img = document.createElement("img");
+  img.src = characterImage;
+  img.alt = characterName;
+  img.className = "character-image";
+
+  const nameDetail = document.createElement("p");
+  nameDetail.className = "character-detail";
+  nameDetail.innerText = "Name:";
+  const name = document.createElement("span");
+  name.className = "atribute";
+  name.innerText = characterName;
+  nameDetail.appendChild(name);
+
+  const statusDetail = document.createElement("p");
+  statusDetail.className = "character-detail";
+  statusDetail.innerText = "Status:";
+  const status = document.createElement("span");
+  status.className = "atribute";
+  status.innerText = characterStatus;
+  statusDetail.appendChild(status);
+
+  const speciesDetail = document.createElement("p");
+  speciesDetail.className = "character-detail";
+  speciesDetail.innerText = "Species:";
+  const species = document.createElement("span");
+  species.className = "atribute";
+  species.innerText = characterSpecies;
+  speciesDetail.appendChild(species);
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.innerText = "Delete";
+  deleteBtn.addEventListener("click", () => {
+    console.log("Deleted item");
+  });
+
+  card.appendChild(img);
+  card.appendChild(nameDetail);
+  card.appendChild(statusDetail);
+  card.appendChild(speciesDetail);
+  card.appendChild(deleteBtn);
+
+  return card;
+};
 
 fetch("https://rickandmortyapi.com/api/character")
   .then((response) => response.json())
   .then((data) => {
-    data.results.forEach((character) => {
-      const card = document.createElement("div");
-      card.className = "card";
-      card.innerHTML = `
-        <img 
-          src=${character.image}
-          alt=${character.name}
-          class="character-image">
-        <p class="character-detail">
-          Name: <span class="atribute">${character.name}</span>
-        </p>
-        <p class="character-detail">
-          Status: <span class="atribute">${character.status}</span>
-        </p>
-        <p class="character-detail">
-          Species: <span class="atribute">${character.species}</span>
-        </p>
-      `
+    characters = data.results;
+    displayCharacters(characters);
+  });
 
-      charactersContainer.appendChild(card);
-    });
-  })
+const displayCharacters = (characters) => {
+  charactersContainer.innerHTML = "";
+  characters.forEach((character) => {
+    charactersContainer.appendChild(
+      createCharacter(
+        character.image,
+        character.name,
+        character.status,
+        character.species
+      )
+    );
+  });
+};
+
+characterSearch.addEventListener("input", (e) => {
+  console.log("Typing:", e.target.value);
+  let search = e.target.value.toLowerCase();
+
+  let filteredCharacters = characters.filter((character) =>
+    character.name.toLowerCase().includes(search)
+  );
+
+  displayCharacters(filteredCharacters);
+});
+
+console.log(characterSearch);
